66a09a9e5bcce641b5bf867c1b70122aa2d8977f
EthantheServer <144978230+EthantheServer@users.noreply.github.com>
2024-02-13 22:15:04 +0800

    Update altedtitle.js

--- altedtitle.js
+++ altedtitle.js
@@ -1,5 +1,4 @@
-var img = document.getElementsByTagName("img");
-
-for (var i = 0; i < img.length; i++) {
+const img = document.querySelectorAll("img");
+for (let i = 0; i < img.length; i++) {
   img[i].title = img[i].alt;
 }
